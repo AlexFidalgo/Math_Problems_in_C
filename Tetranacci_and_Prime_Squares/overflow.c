89fdb5f31f9cce4a1c05fbf72b875e2667e23fff
Alex Fidalgo <67179630+AlexFidalgo@users.noreply.github.com>
2023-04-03 13:19:59 -0300

    Update overflow.c

--- Tetranacci_and_Prime_Squares/overflow.c
+++ Tetranacci_and_Prime_Squares/overflow.c
@@ -1,36 +1,24 @@
+#include "overflow.h"
 #include <stdio.h>
 #include <stdint.h>
 
-uint64_t tetranacci(uint64_t n) {
-    if (n == 0 || n == 1 || n == 2) {
-        return 0;
-    }
-    if (n == 3) {
-        return 1;
-    }
+uint64_t max_tetranacci() {
+    int i = 4;
     uint64_t tn_1 = 0;
     uint64_t tn_2 = 0;
     uint64_t tn_3 = 0;
     uint64_t tn_4 = 1;
-    for (uint64_t i = 4; i <= n; i++) {
+    
+    while (1) {
         uint64_t tn = tn_1 + tn_2 + tn_3 + tn_4;
         tn_1 = tn_2;
         tn_2 = tn_3;
         tn_3 = tn_4;
         tn_4 = tn;
         if (tn > UINT64_MAX / 2) { // check for overflow
-            printf("Overflow at n = %llu\n", n);
-            return UINT64_MAX;
+            /*printf("Overflow at n = %llu\n", n);*/
+            return i;
         }
+        i = i + 1;
     }
-    return tn_4;
-}
-
-int main() {
-    uint64_t n = 0;
-    while (tetranacci(n) <= UINT64_MAX / 2) {
-        n++;
-    }
-    printf("The smallest n whose tetranacci does not exceed 2^64-1 is %llu\n", n);
-    return 0;
 }
